11c9cbc32ea09f28c8374e451bcc3331c5204eda
gonz_tyler <gonzaloescolartyler@gmail.com>
2024-10-18 11:44:09 +0100

    Fixed user model configuration

--- mouse_colony_management/settings.py
+++ mouse_colony_management/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mouse_colony_management',
     'website',
     'whitenoise.runserver_nostatic',
 ]
