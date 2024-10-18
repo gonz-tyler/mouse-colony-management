3088ee37df5e0589dcc47cf0195128ba07de63b8
Gonzalo Eusebio Escolar Tyler <84409108+gonz-tyler@users.noreply.github.com>
2024-10-18 19:14:43 +0100

    Update admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Profile)
+admin.site.register(Team)
+admin.site.register(TeamMembership)
+admin.site.register(Mouse)
+admin.site.register(Strain)
+admin.site.register(Cage)
+admin.site.register(Breed)
+admin.site.register(BreedingHistory)
+admin.site.register(Genotype)
+admin.site.register(Phenotype)
