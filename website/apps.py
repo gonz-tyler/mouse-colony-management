aa2e1382857b9aff15ea6d968f3f9ecaa5d06d73
Gonzalo Eusebio Escolar Tyler <84409108+gonz-tyler@users.noreply.github.com>
2024-10-29 10:57:19 +0000

    Update apps.py

--- website/apps.py
+++ website/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'website'
-
-    def ready(self):
-        # Import your models and ensure groups are created
-        from .models import Profile
-        Profile.create_default_groups()
