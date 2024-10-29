107f15f0e4ca627a49123ca1941cedc4c752f1cd
Gonzalo Eusebio Escolar Tyler <84409108+gonz-tyler@users.noreply.github.com>
2024-10-29 01:02:06 +0000

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='index'),  # Index page
+    path('legal/terms-of-service/', views.terms_of_service, name='terms_of_service'), # legal
+    path('legal/privacy-policy/', views.privacy_policy, name='privacy_policy'), # legal
+    path('', views.home_view, name='index'),  # Index page
     path('login/', auth_views.LoginView.as_view(), name='login'),  # Login page
-    path('logout/', views.logout_user, name="logout"),
-    path('create-user/', views.create_user, name='create_user'),
+    path('register/', views.register, name='register'), # Register page
+    path('logout/', views.logout_user, name="logout_user"),
+    path('genetic-tree/<int:mouse_id>/', views.genetic_tree, name='genetic_tree'), # Genetic Tree page
 ]
